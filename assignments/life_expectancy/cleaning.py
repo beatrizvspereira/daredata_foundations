949a5245e02578851eee49e18b71b41acd84e324
beatrizvspereira <beatrizventuraspereira@hotmail.com>
2024-06-05 00:49:13 +0100

    5.06 00:49

---
+++ assignments/life_expectancy/cleaning.py
@@ -0,0 +1,44 @@
+# pylint: disable=line-too-long
+import argparse
+import pandas as pd
+
+def clean_data(country="PT"):
+    """
+    Load, clean, and save life expectancy data for Portugal.
+    """
+    # Load the data
+    df = pd.read_csv(
+        '/Users/bia/Desktop/DareData/daredata_foundations/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv', 
+        sep='\t'
+    )
+    print("Initial DataFrame:")
+    print(df.head())
+    print("\n")
+
+    # Split the first column into multiple columns
+    df[['unit', 'sex', 'age', 'geo']] = df.iloc[:, 0].str.split(',', expand=True)
+    df = df.drop(df.columns[0], axis=1)
+
+    df = df.melt(id_vars=['unit', 'sex', 'age', 'geo'], var_name='year', value_name='value')
+    df['year'] = pd.to_numeric(df['year'], errors='coerce')
+    df['value'] = pd.to_numeric(df['value'], errors='coerce')
+    df.dropna(subset=['value'], inplace=True)
+    df = df[df['geo'] == 'PT']
+    print("Cleaned DataFrame:")
+    print(df.head())
+    print(f"DataFrame shape: {df.shape}")
+
+    # Save the DataFrame
+    df.to_csv(
+        '/Users/bia/Desktop/DareData/daredata_foundations/assignments/life_expectancy/data/pt_life_expectancy.csv', 
+        index=False
+    )
+
+#if __name__ == "__main__":
+#    clean_data()
+
+if __name__ == "__main__":  # pragma: no cover
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--country", default="PT", help="Country code to filter the data")
+    args = parser.parse_args()
+    clean_data(args.country)
--- assignments/life_expectancy/tests/test_cleaning.py
+++ assignments/life_expectancy/tests/test_cleaning.py
@@ -4,10 +4,9 @@ import pandas as pd
 from life_expectancy.cleaning import clean_data
 from . import OUTPUT_DIR
 
-
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data()
+    clean_data(country="PT")
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
