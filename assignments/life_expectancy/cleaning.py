af41daa541e60fa8d195d0ce0864cd397400fd78
beatrizvspereira <beatrizventuraspereira@hotmail.com>
2024-06-06 00:10:13 +0100

    Pytest passed

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -1,38 +1,41 @@
 # pylint: disable=line-too-long
+
 import argparse
 import pandas as pd
 
 def clean_data(country="PT"):
     """
-    Load, clean, and save life expectancy data for Portugal.
+    Load, clean, filter and save life expectancy data for Portugal. The cleaning process envolves unpivoting the date to long format,
+    ensure 'year' is an int, 'value' is a float and clean the errors. 
     """
     df = pd.read_csv(
         '/Users/bia/Desktop/DareData/daredata_foundations/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv', 
         sep='\t'
     )
-    print("Initial DataFrame:")
-    print(df.head())
-    print("\n")
+    print(df.shape)
 
     df[['unit', 'sex', 'age', 'geo']] = df.iloc[:, 0].str.split(',', expand=True)
     df = df.drop(df.columns[0], axis=1)
+    print(df.shape)
 
     df = df.melt(id_vars=['unit', 'sex', 'age', 'geo'], var_name='year', value_name='value')
+    print(df.shape)
+
+
     df['year'] = pd.to_numeric(df['year'], errors='coerce')
+    
+    df['value'] = df['value'].str.replace('e', '', regex=False)
     df['value'] = pd.to_numeric(df['value'], errors='coerce')
     df.dropna(subset=['value'], inplace=True)
     df = df[df['geo'] == 'PT']
-    print("Cleaned DataFrame:")
-    print(df.head())
-    print(f"DataFrame shape: {df.shape}")
+    df.rename(columns={'geo': 'region'}, inplace=True)  
 
+    print(df.shape)
+  
     df.to_csv(
         '/Users/bia/Desktop/DareData/daredata_foundations/assignments/life_expectancy/data/pt_life_expectancy.csv', 
         index=False
     )
-
-#if __name__ == "__main__":
-#    clean_data()
 
 if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser()
